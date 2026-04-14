5c6164441cc983beec96ffcd5be7b823e2616bc2
ChulwonPark-econ <inuuniw@gmail.com>
2026-04-14 07:08:57 +0000

    Deploying to gh-pages from @ ChulwonPark-econ/ChulwonPark-econ.github.io@8fcacd749768ca7c3aaf32bc5bd2c5f049caa94f 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -9,7 +9,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "inuuniw-econ/inuuniw-econ.github.io",
+    "data-repo": "ChulwonPark-econ/ChulwonPark-econ.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
